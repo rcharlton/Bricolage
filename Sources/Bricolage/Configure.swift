d2620b02731336dc62e039a02ba42faab1f3c0a7
Robin Charlton <mail@robincharlton.me>
2021-04-18 13:14:17 +0100

    Simplified func configure.

--- Sources/Bricolage/Configure.swift
+++ Sources/Bricolage/Configure.swift
@@ -7,8 +7,8 @@
 ///   - resolve: An expression to provide the initial instance. Value types are copied.
 ///   - closure: An expression to mutate the provided instance.
 /// - Returns:  The configured instance.
-public func configure<T>(_ resolve: @autoclosure () -> T, with closure: (inout T) -> Void) -> T {
-    var instance = resolve()
+public func configure<T>(_ instance: T, with closure: (inout T) -> Void) -> T {
+    var instance = instance
     closure(&instance)
     return instance
 }
