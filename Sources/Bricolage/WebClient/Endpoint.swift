143c2fee80ebd4f3c4583ac38725a97905425122
Robin Charlton <mail@robincharlton.me>
2022-11-03 11:58:56 +0000

    Minor formatting tweak.

--- Sources/Bricolage/WebClient/Endpoint.swift
+++ Sources/Bricolage/WebClient/Endpoint.swift
@@ -11,7 +11,6 @@ public protocol RequestProviding {
 }
 
 public protocol ResponseDecoding {
-
     associatedtype Success
     associatedtype Failure
 
@@ -27,49 +26,39 @@ public protocol ResponseDecoding {
 // MARK: -
 
 public extension ResponseDecoding {
-
     var successStatusCodes: AnyCollection<Int> { AnyCollection(200..<400) }
 
     var decoder: Decoding { JSONDecoder() }
-
 }
 
 // MARK: -
 
 public extension ResponseDecoding where Success: Decodable {
-
     func decodeSuccess(from data: Data, response: HTTPURLResponse) throws -> Success {
         try decoder.decode(Success.self, from: data)
     }
-
 }
 
 // MARK: -
 
 public extension ResponseDecoding where Failure: Decodable {
-
     func decodeFailure(from data: Data, response: HTTPURLResponse) throws -> Failure {
         try decoder.decode(Failure.self, from: data)
     }
-
 }
 
 // MARK: -
 
 public extension ResponseDecoding where Success == Void {
-
     func decodeSuccess(from data: Data, response: HTTPURLResponse) throws -> Success {
         ()
     }
-
 }
 
 // MARK: -
 
 public extension ResponseDecoding where Failure == Void {
-
     func decodeFailure(from data: Data, response: HTTPURLResponse) throws -> Failure {
         ()
     }
-
 }
